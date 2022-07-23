3795d4418171477a5243016882d7bb79e330ef82
30671 <3067132996@qq.com>
2022-07-23 23:46:46 +0800

    7/23

--- Chapter2/DNode/DNode.cpp
+++ Chapter2/DNode/DNode.cpp
@@ -7,6 +7,85 @@
     @date:          2022/7/23
     @version:       1.0
 */
-
-
 #include "DNode.h"
+
+bool InitDLinkList(DLinkList &L) {
+    L = (DLinkList) malloc(sizeof(DNode));
+    if (L == nullptr) {
+        return false;
+    }
+    L->prior = nullptr;
+    L->next = nullptr;
+
+    return true;
+}
+
+bool InsertNextDNode(DNode *p, DNode *s) {
+    // 原先的***  p <=> p->next ****
+    // 插入到p之后 变为 *** p <=> s <=> p->next ***
+
+    if (p == nullptr || s == nullptr) {
+        return false;
+    }
+
+    s->next = p->next;  // 将结点s插入到结点p之后
+
+    if (p->next != nullptr) {
+        p->next->prior = s;
+    }
+    s->prior = p;
+    p->next = s;
+}
+
+
+bool DeleteNextDNode(DNode *p) {
+    if (p == nullptr) {
+        return false;
+    }
+    DNode *q = p->next;
+
+    if (q == nullptr) {
+        return false;
+    }
+
+    p->next = q->next;
+
+    if (q->next != nullptr) {
+        q->next->prior = p;
+    }
+    free(q);
+
+    return true;
+}
+
+void DestoryDLinkList(DLinkList &L) {
+    // 每次删除头结点的后一个结点
+    // 直到删除结束
+
+    while (L->next != nullptr) {
+        DeleteNextDNode(L);
+    }
+    free(L);
+    L = nullptr;
+}
+
+void TraverseDLinkList(DNode *p) {
+    // 后向遍历
+    while (p != nullptr) {
+        p = p->next;
+    }
+
+    // 前向遍历
+    //    while (p != nullptr) {
+    //        p = p->prior;
+    //    }
+
+    // 前向遍历(跳过头结点)
+    //    while (p->prior != nullptr) {
+    //        p = p->prior;
+    //    }
+}
+
+void TestDLinkList() {
+
+}
--- Chapter2/DNode/DNode.h
+++ Chapter2/DNode/DNode.h
@@ -20,4 +20,48 @@ typedef struct DNode {
 } DNode, *DLinkList;
 
 
+/**
+ * 初始化带头结点双链表
+ * @param L
+ * @return
+ */
+
+bool InitDLinkList(DLinkList &L);
+
+/**
+ * 将结点s插入到p结点之后
+ * @param p
+ * @param s
+ * @return
+ */
+bool InsertNextDNode(DNode *p, DNode *s);
+
+
+/**
+ * 删除p结点的后继节点
+ * @param p
+ * @return
+ */
+bool DeleteNextDNode(DNode *p);
+
+
+/**
+ * 删除整个双链表
+ * @param L
+ */
+void DestoryDLinkList(DLinkList &L);
+
+/**
+ * 遍历双链表
+ *      只需要给任意一个结点则可遍历整个链表
+ * @param L
+ */
+void TraverseDLinkList(DNode* p);
+
+/**
+ * 功能测试函数
+ */
+void TestDLinkList();
+
+
 #endif //DATASTRUCTURE_DNODE_H
