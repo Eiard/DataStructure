dde9f5e0a53ac5a076e5f7783cc6d8f50feacf4c
30671 <3067132996@qq.com>
2022-07-31 00:20:27 +0800

    7/23

--- Chapter3/SqQueue/SqQueue.cpp
+++ Chapter3/SqQueue/SqQueue.cpp
@@ -3,7 +3,7 @@
     @projectName:   DataStructure 
     @FileName:      SqQueue
     @author:        30671
-    @description:   TODO 
+    @description:   DONE : 静态队列
     @date:          2022/7/30
     @version:       1.0
 */
@@ -31,8 +31,12 @@ bool SqQueueIsFull(SqQueue Q) {
 bool EnSqQueue(SqQueue &Q, ElemType x) {
     if (SqQueueIsFull(Q))
         return false; // 队满 报错
-    // 队尾存入数据 存入后下标后移
-    Q.data[Q.rear++] = x;
+
+    // 存入数据
+    Q.data[Q.rear] = x;
+    // 取余 (取模)
+    Q.rear = (Q.rear + 1) % MaxSize;
+
     return true;
 }
 
